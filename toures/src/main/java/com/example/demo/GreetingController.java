74824680a0d0dc94219703a3c49170d19ded993a
sergionet17@gmail.com <sergionet17@gmail.com>
2017-09-02 16:55:41 -0500

    subir toures

---
+++ toures/src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hola_espanol, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+    
+    @RequestMapping("/test")
+    public Greeting greeting_test(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+    
+    @RequestMapping("/lista")
+    public List<Greeting> greeting_test() {
+        List<Greeting> lista =  new ArrayList<Greeting>();
+        Greeting greeting = new Greeting(0, "test_lista");
+        Greeting greeting_2 = new Greeting(1, "parcial");
+        lista.add(greeting_2);
+        lista.add(greeting);
+        return lista;
+    }
+    
+}
