ed9f6a182d44a1d60b5af196635d27a774d65e8f
Sergio <sergionet17@gmail.com>
2017-10-27 10:16:10 -0500

    cambio del 27 de octubre se eliminan las carpetas que no aportan

--- scrip_subida.sh
+++ scrip_subida.sh
@@ -2,7 +2,7 @@
 
 git  add .
 
-git commit -m "cambio con script"
+git commit -m "cambio del 27 de octubre se eliminan las carpetas que no aportan"
 
 git push --set-upstream https://github.com/sergionet17/toures_ballon  master
 
